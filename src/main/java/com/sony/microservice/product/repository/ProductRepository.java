fb45150e7bb6808ecdb6e0ba63589d44a7fe12c1
Sony mathew <sonym3@gmail.com>
2025-01-02 14:48:02 -0400

    log changes

--- src/main/java/com/sony/microservice/product/repository/ProductRepository.java
+++ src/main/java/com/sony/microservice/product/repository/ProductRepository.java
@@ -2,6 +2,6 @@ package com.sony.microservice.product.repository;
 
 import com.sony.microservice.product.model.Product;
 import org.springframework.data.mongodb.repository.MongoRepository;
-
+//this is our repo
 public interface ProductRepository extends MongoRepository<Product, String> {
 }
